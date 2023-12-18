3fd8bf63f316d38eba2b6f257ad9a910a6229fb1
ghazal <ghazalfatal.02@gmail.com>
2023-12-18 15:28:35 +0200

    commit-4

--- database/orphan.sql
+++ database/orphan.sql
@@ -1,77 +1,221 @@
-<!DOCTYPE html>
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 18, 2023 at 10:41 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-<?php
-include('../../connection.php');
-if (isset($_SESSION['id'])) {
-  header('location:../profileDriver.php');
-}
-$error="";
-if (isset($_POST['submit'])) {
-  session_start();
-  $id = $_SESSION['id'];
-  // echo $id;  
-  $ida = $_POST['adminid'];
-  $name= $_POST['name'];
-  $location = $_POST['location'];
-  $records = $_POST['records'];
-  $query = "INSERT INTO `branch`(`id`, `adminid`, `name`, `location`, `records`) 
-    VALUES (NULL,'$ida','$name','$location','$records')";
-    if (mysqli_query($conn, $query)) {
-        $_SESSION['insert'] = "insert success";
-        header("location:../profileDriver.php");
-        exit();
-      } else {
-        $error = "Error: " . $query . "<br>" . mysqli_error($conn);
-      }
-    }
-    if (isset($_POST['return'])) {
-        header("location:../profileDriver.php");
-      }
-      
-      ?>
-      <html>
-        <head>
-          <meta charset="utf-8" />
-          <title>Create Trip | NEW</title>
-          <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
-          <link rel="stylesheet" href="formes.css" />
-          <link rel= " stylesheet "href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
-        </head>
-        <body>
-              
-              <nav class="navbar">
-                  <h1 class="logo">Give & Thrive</h1>
-                  <ul class="nav-links">
-                      <li class="active"> <a href="#"> Home</a></i></li>
-                      <li> <a href="#"> Services</a></i></li>
-                      <li> <a href="#"> ABOUT</a></i></li>
-                      <li class=""> <a href="#"> Contact-US</a></i></li>
-                      
-                  </ul>
-              </nav>
-              
-    <div class="container">
-      <h1 class="form-title">Add New Branch </h1>
-      <form action="./formtrip.php" method =post >
-        <div class="main-user-info"> 
-        <div class="user-input-box1">
-        <label for="adminid">AdminID:
-            <input name="adminid" type="text"></label></div>
-        <div class="user-input-box2">
-        <label for="name">Name: 
-              <input name="name" type="text"></label></div>
-        <div class="user-input-box3">      
-        <label for="">Location:
-            <input name="location" type="text"></label></div>
-        <div class="user-input-box4">
-            <label for="time">Records:
-            <input name="records" type="text"></label></div> 
-            <p class = "err"><?php echo $error; ?></p>
-        </div>
-        <div class="form-submit-btn">
-          <input type="submit" value="Add Branch" name = submit>
-          <input type="submit" value="Return" name = return>
-        </div>       
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-      </form>
-      </div>
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `orphan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addorphan`
+--
+
+CREATE TABLE `addorphan` (
+  `idorphan` int(50) NOT NULL,
+  `nameorphan` varchar(50) NOT NULL,
+  `age` int(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `branch` varchar(50) NOT NULL,
+  `amount` int(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `addorphan`
+--
+
+INSERT INTO `addorphan` (`idorphan`, `nameorphan`, `age`, `gender`, `branch`, `amount`, `description`) VALUES
+(1, 'bilal', 10, 'male', 'tripoli', 1000, 'need an urgent surgery ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branch`
+--
+
+CREATE TABLE `branch` (
+  `id` int(50) NOT NULL,
+  `adminid` int(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `records` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `branch`
+--
+
+INSERT INTO `branch` (`id`, `adminid`, `name`, `location`, `records`) VALUES
+(1, 0, 'branch1', 'location1', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `messageid` varchar(50) NOT NULL,
+  `userid` varchar(50) NOT NULL,
+  `message` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gender`
+--
+
+CREATE TABLE `gender` (
+  `gender` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `gender`
+--
+
+INSERT INTO `gender` (`gender`) VALUES
+('female'),
+('male');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`role`) VALUES
+('admin'),
+('user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `image` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `gender`, `password`, `role`, `email`, `description`, `image`) VALUES
+(1, 'ghazal ', 'female', 'Ghazal123#', 'admin', 'ghazalfattal.02@gmail.com ', 'hey', ''),
+(2, 'shaza', 'female', 'Ghazal123#', 'admin', 'shazi@gmail.com', '', ''),
+(3, 'sally', 'female', 'Ghazal123#', 'admin', 'sall@gmail.com', '', ''),
+(4, 'nancy', 'female', 'Ghazal123#', 'user', 'nancy@gmail.com', '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addorphan`
+--
+ALTER TABLE `addorphan`
+  ADD PRIMARY KEY (`idorphan`);
+
+--
+-- Indexes for table `branch`
+--
+ALTER TABLE `branch`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gender`
+--
+ALTER TABLE `gender`
+  ADD PRIMARY KEY (`gender`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_ibfk_1` (`gender`),
+  ADD KEY `user_ibfk_2` (`role`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `addorphan`
+--
+ALTER TABLE `addorphan`
+  MODIFY `idorphan` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `branch`
+--
+ALTER TABLE `branch`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `branch`
+--
+ALTER TABLE `branch`
+  ADD CONSTRAINT `branch_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`gender`) REFERENCES `gender` (`gender`),
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`role`) REFERENCES `role` (`role`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

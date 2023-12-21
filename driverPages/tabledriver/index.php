0310ef7974dcdd938d67fa91c1e4f86df3fe542c
shazaatieh <shazaatieh21@gmail.com>
2023-12-21 17:27:31 +0200

    change the msg to white

--- driverPages/tabledriver/index.php
+++ driverPages/tabledriver/index.php
@@ -28,10 +28,10 @@ include "connection.php";
     <?php
     if (isset($_GET["msg"])) {
       $msg = $_GET["msg"];
-      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert" style="color:white; padding:10px 15px;">
       ' . $msg . '
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>';
+    </div>';
     }
     ?>
     <main class="table">

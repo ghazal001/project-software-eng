1a6fd0c9d49c74b53fd54fcf59e70f9616a67ce2
ghazal <ghazalfatal.02@gmail.com>
2023-12-21 20:44:00 +0200

    css of edit orphan done

--- driverPages/editorphan.php
+++ driverPages/editorphan.php
@@ -43,45 +43,36 @@ $sql = "UPDATE `addorphan` SET
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
+  <link rel="stylesheet" href="./editorphan.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
 
   <title> </title>
 </head>
 
 <body>
-  <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: red;">
-    Editorphan
-  </nav>
 
-  <div class="container">
-    <div class="text-center mb-4">
-      <h3>Edit </h3>
-      <p class="text-muted">Click update after changing any information</p>
-    </div>
+  
+      <h3 class="her" >Edit Orphan</h3>
+      <p class="her">Click update after changing any information</p>
 
+    <div class="container">
+      <form action=""method="post">
+        <div class="all">
     <?php
     $sql = "SELECT * FROM `addorphan` WHERE `idorphan` =  $id";
     $result = mysqli_query($conn, $sql);
     $orph = mysqli_fetch_assoc($result);
     ?>
 
-    <div class="container d-flex justify-content-center">
+    <div class="row1">
       <form action="" method="post" style="width:50vw; min-width:300px;">
-        <div class="row mb-3">
-
-          <div class="col">
-            <label class="form-label">Name:</label>
+            <label class="lab">Name:</label>
             <input type="text" class="form-control" name="nameorphan" value="<?php echo $orph['nameorphan'] ?>">
-          </div>
-        </div>
-
-        <div class="mb-3">
-          <label class="form-label">age:</label>
+    
+          <label class="lab">age:</label>
           <input type="text" class="form-control" name="age" value="<?php echo $orph['age'] ?>">
-        </div>
 
         <!-- <div class="form-group mb-3">
           <label>Gender:</label>
@@ -93,12 +84,11 @@ $sql = "UPDATE `addorphan` SET
           <label for="female" class="form-input-label">Female</label>
         </div> -->
         
-        <div class="mb-3">
-          <label class="form-label">gender:</label>
+          <label class="lab">gender:</label>
           <input type="to" class="form-control" name="gender" value="<?php echo $orph['gender'] ?>">
-        </div>
-        <div class="mb-3">
-          <label class="form-label">branch_name:</label>
+    
+        
+          <label class="lab">branch_name:</label>
           <select name="location" id="" class="form-control">
             <?php
               $loc  =$orph['locationID'];
@@ -114,17 +104,16 @@ $sql = "UPDATE `addorphan` SET
             ?>
           </select>
         </div>
-        <div class="mb-3">
-          <label class="form-label">amount</label>
+        <div class="row2">
+          <label class="lab">amount</label>
           <input type="number" class="form-control" name="amount" value="<?php echo $orph['amount'] ?>">
-        </div>
-        <div class="mb-3">
-          <label class="form-label">description</label>
+
+          <label class="lab">description</label>
           <input type="text" class="form-control" name="description" value="<?php echo $orph['description'] ?>">
         </div>
         
 
-        <div>
+        <div class="row4">
           <button type="submit" class="btn btn-success" name="submit">Update</button>
           <a href="profileDriver.php" class="btn btn-danger">Cancel</a>
         </div>

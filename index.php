ac28c22546a1fe45881ba52ded853b6e0412ab88
shazaatieh <shazaatieh21@gmail.com>
2023-12-24 13:50:13 +0200

    login 

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Give and Thrive</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="./css/styleHome.css" />
 </head>
 
@@ -38,7 +40,7 @@
                             <a href="#joinSection">Sponsorships</a>
                         </li>
                         <li>
-                        <a href="./login/index.php">Login/Register</a>
+                        <a href="./login/index.php"><i class="fa fa-user fa-lg"></i>  Login/Register</a>
                         </li>
                     </ul>
                 </nav>

b0eb05de06a90e0d896e04c9086ec0bb5889e9c6
shazaatieh <shazaatieh21@gmail.com>
2023-12-24 14:11:40 +0200

    home 

--- adminPages/tabledriver/add_new.php
+++ adminPages/tabledriver/add_new.php
@@ -34,7 +34,7 @@ echo "Failed: " . mysqli_error($conn);
 
    <!-- Bootstrap -->
    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
-   <link rel="stylesheet"href="./addnewStyle.css">
+   <link rel="stylesheet"href="./addnewstyle.css">
    <!-- Font Awesome -->
    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
 
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Give and Thrive</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <link rel="stylesheet" href="./css/styleHome.css" />
+    <link rel="stylesheet" href="./css/stylehome.css" />
 </head>
 
 <body>
